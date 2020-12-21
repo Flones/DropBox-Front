c3886dd99ac35733987aa90283bfff96622819d1
Ulrich-Nelson <ulrichnelsontapinfo@yahoo.fr>
2020-12-21 20:17:28 +0100

    Stylisation de la page de connexion et activation du lien

--- src/components/login/LoginPage.tsx
+++ src/components/login/LoginPage.tsx
@@ -4,6 +4,8 @@ import { LockOutlined } from '@material-ui/icons'
 import React, { PureComponent } from 'react'
 import { Link } from 'react-router-dom'
 import createStylesLogin, { Createlogin } from './loginStyles'
+import { GoogleLogin } from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
 
 interface P{}
 interface S{} 
@@ -13,14 +15,18 @@ class LoginPage extends PureComponent<P & WithStyles<Createlogin>, S> {
   render() {
     const {classes} = this.props 
 
+    //A faire plutard pour la connexion avec Google et Facebook
+    const responseGoogle = async () =>{}
+    const responseFacebook = async () =>{}
+
     return (
-       <Grid>
+       <Grid container  justify="center" className={classes.root}>
+            <Grid>
             <Paper elevation={10} className={classes.paper}>
                 <Grid justify="center">
                      <Avatar className={classes.avatar}><LockOutlined/></Avatar>
-                    <h2 className={classes.titleConnexion}>Connexion</h2>
+                    <h2 className={classes.titleConnexion}>Connexion✍</h2>
                 </Grid>
-                <form>
                 <TextField 
                 required
                 fullWidth
@@ -41,16 +47,44 @@ class LoginPage extends PureComponent<P & WithStyles<Createlogin>, S> {
                 <Button type='submit' color='primary' variant="contained" className={classes.Bouton} fullWidth>Connexion</Button>
                 <Typography >
                      <Link to="/motdePasseOublie">
-                        Forgot password ?
-                </Link>
+                        <span className={classes.Motdepasseoublie}>Forgot password ?</span>
+                      </Link>
                 </Typography>
-                <Typography > Créer mon compte
-                     <Link to="/inscription">
-                        Inscription
+                <Grid container>
+                     <div className={classes.titleMessage}> Si vous n'avez pas de compte &nbsp;</div> <br/>
+                     <Link to="/inscription" >
+                       <div className={classes.Connexion}> Inscription</div>
                 </Link>
-                </Typography>
-                </form>
+                </Grid>
             </Paper>
+            </Grid>
+              
+            <Grid>
+            <Paper elevation={10} className={classes.paper}>
+                <Grid justify="center">
+                     <Avatar className={classes.avatar}>réseaux sociaux</Avatar>
+                    <h2 className={classes.titleConnexion}>Réseaux sociaux</h2>
+                </Grid>
+
+                <div className={classes.reseauButton}>
+                <GoogleLogin
+                  clientId="Par nos identifiant nodejs"
+                  buttonText="Se connecter avec google"
+                  onSuccess={responseGoogle}
+                  cookiePolicy={'single_host_origin'}
+                  />
+                </div>                
+                <div className={classes.reseauButton}>   
+                 <FacebookLogin
+                    appId="Par les identifiant de FaceBook"
+                    autoLoad={false}
+                    fields="name,email,picture"
+                    callback={responseFacebook}
+                    icon="fa-facebook"
+                      />
+                </div>
+            </Paper>
+            </Grid>
         </Grid>
       )
   }
