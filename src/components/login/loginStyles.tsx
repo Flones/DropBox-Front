71ba623f624daef06d8cd158bb5ba82a33527fa5
Ulrich-Nelson <ulrichnelsontapinfo@yahoo.fr>
2020-12-21 20:18:35 +0100

    Stylisation de la page de connexion et activation du lien

--- src/components/login/loginStyles.tsx
+++ src/components/login/loginStyles.tsx
@@ -2,7 +2,7 @@ import { createStyles, Theme } from "@material-ui/core";
 
 export type Createlogin = ("root"|"paper"|"avatar"|"Bouton"
 |"GrisAlign"|"titleConnexion"|"reseauButton"|"Connexion"
-|"titleMessage"|"Motdepasseoublie"|"marginpassword") ;;
+|"titleMessage"|"Motdepasseoublie") ;;
 
 const createStylesLogin= (theme: Theme) => createStyles<Createlogin, {}>({
   root:{
@@ -60,10 +60,6 @@ const createStylesLogin= (theme: Theme) => createStyles<Createlogin, {}>({
         fontSize : '15px',
 
         },
-        marginpassword:{
-          marginTop:'30px',
-        },
-
 });
 
 export default  createStylesLogin
